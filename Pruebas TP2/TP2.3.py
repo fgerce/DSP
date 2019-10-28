33756d2046a16dadf45cd0745065d6d5dffa591e
fgerce <fgerce@gmail.com>
2019-10-28 02:50:19 -0300

    TP2 terminado

--- Pruebas TP2/TP2.3.py
+++ Pruebas TP2/TP2.3.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  9 19:35:03 2019
+Created on Mon Oct 28 01:19:14 2019
 
 @author: fede
 """
-
-import sys
-
-sys.path.append('/home/fede/PDS/DSP-Entregas/Modulos')
-
-import instrumentos as ins
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sig
+from scipy.fftpack import fft, fftshift
 
 N  = 1000 # muestras
 fs = 1000 # Hz
@@ -27,12 +22,15 @@ p0 = 0     # radianes
 a0 = 2      # Volts
 
 df = np.random.uniform(-2,2, k)
-fn = f0 + df
+fn = f0 + df*fs/N
 
 Ts = 1/fs
 tt = np.linspace(0, (N-1)*Ts, N)
 
+x = np.transpose(np.vstack([a0*np.sin(2*np.pi*ff*tt) for ff in fn]))
+
 ventana = [sig.boxcar(N), sig.bartlett(N), sig.hann(N), sig.blackman(N), sig.flattop(N)]
+
 V = len(ventana)
 sesgo = np.zeros(V)
 var = np.zeros(V)
@@ -41,24 +39,19 @@ prom = np.zeros(V)
 dist = np.zeros((k,V))
 
 for (vv, this_win) in zip(range(V), ventana):
-       sig = np.transpose(np.vstack([a0*np.sin(2*np.pi*ff*tt+p0)*this_win for ff in fn]))
-       
-       lim = int(np.floor(N/2))
-       ww = np.linspace(0,(N-1)*fs/N, N)
-       complexMat = np.transpose(np.vstack([np.fft.fft(sig[:,ii]) for ii in range(0,k)]))
-       moduloMat = np.abs(complexMat)/lim
-       faseMat = np.angle(complexMat, True)
-       ww = ww[0:lim]
-       moduloMat = moduloMat[0:lim]
-       faseMat = faseMat[0:lim]
-       
-       moduloMat = moduloMat[int(fs/4), :]
-       dist[:,vv] = moduloMat
-       prom[vv] = np.mean(moduloMat)
+       X = np.transpose(np.vstack([x[:,kk]*this_win for kk in range(0,k)]))
+       X = fft(X, axis=0)
+       modX = np.abs(fftshift(X))*2/N
+       center = int(np.floor(N/2))
+       modX = modX[center:N]
+       freq = np.linspace(0, 0.5, len(modX))
+       mod_slice = modX[248:253, :]**2
+       mod_slice = np.mean(mod_slice, axis=0)
+       dist[:,vv] = mod_slice
+       prom[vv] = np.mean(mod_slice)
        sesgo[vv] = prom[vv] - a0
-       var[vv] = np.mean(np.exp2(moduloMat - prom[vv]))
-
-
+       var[vv] = np.mean((mod_slice-prom[vv])**2)
+       
 s1, edge1 = np.histogram(dist[:,0], 20)
 s2, edge2 = np.histogram(dist[:,1], 20)
 s3, edge3 = np.histogram(dist[:,2], 20)
@@ -66,20 +59,14 @@ s4, edge4 = np.histogram(dist[:,3], 20)
 s5, edge5 = np.histogram(dist[:,4], 20)
 
 
-plt.figure("Distribucion lineal")
+plt.figure("Histograma normalizado del estimador")
+plt.title("Histograma normalizado del estimador")
 plt.plot(edge1[0:20], s1, label="rectangular")  
 plt.plot(edge2[0:20], s2, label="barlett")  
 plt.plot(edge3[0:20], s3, label="hanning")  
 plt.plot(edge4[0:20], s4, label="blackman")  
 plt.plot(edge5[0:20], s5, label="flattop")  
+plt.xlabel("Amplitud estimada")
+plt.ylabel("Cantidad de repeticiones (k=200)")
 
 plt.legend()
-
-plt.figure("Distribucion total")
-plt.hist(dist[:,0], label="rectangular")
-plt.hist(dist[:,1], label="barlett")
-plt.hist(dist[:,2], label="hanning")
-plt.hist(dist[:,3], label="blackman")
-plt.hist(dist[:,4], label="flattop")
-
-#Implementar Seaborn!
--- Pruebas TP2/ventanasTP2.2e.py
+++ Pruebas TP2/ventanasTP2.2e.py
@@ -5,4 +5,43 @@ Created on Mon Oct 28 00:24:33 2019
 
 @author: fede
 """
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sig
+from scipy.fftpack import fft, fftshift
 
+N = 1000
+fs = 1000
+Ts = 1/fs
+
+tt = np.linspace(0, (N-1)*Ts, N)
+
+ventana = sig.flattop(N)
+
+daux = np.asarray([1, 1, 1])
+d = np.asarray([0.8, 0.9, 1])
+f1 = fs/4 + daux*0.5*fs/N
+f2 = f1 + d*(fs/N)
+
+a2 = 1
+
+x1 = np.transpose(np.vstack([np.sin(2*np.pi*fi*tt) for fi in f1]))
+x2 = np.transpose(np.vstack([a2*np.sin(2*np.pi*fi*tt) for fi in f2]))
+
+x = x1 + x2
+for aux in range(len(d)):
+       x[:,aux] = x[:,aux] * ventana
+
+X = fft(x, axis=0)
+modX = np.abs(fftshift(X))*2/N
+center = int(np.floor(N/2))
+modX = modX[center:N]
+modX = 20*np.log10(modX)
+freq = np.linspace(0, 0.5, len(modX))
+plt.figure("Modulo bitonal", figsize=(10,10))
+plt.plot(freq, modX)
+plt.grid()
+plt.xlim(0.248,0.28)
+plt.xlabel("Frecuencia normalizada")
+plt.ylabel("Amplitud en dB")
